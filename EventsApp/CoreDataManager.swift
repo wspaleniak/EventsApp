3bfc4e1958b58aeb7885dfec1c2428c5f783e656
wspaleniak <w.spaleniak@o2.pl>
2023-02-22 20:11:58 +0100

    - add CoreData model & CoreDataManager class

---
+++ EventsApp/CoreDataManager.swift
@@ -0,0 +1,50 @@
+//
+//  CoreDataManager.swift
+//  EventsApp
+//
+//  Created by Wojciech Spaleniak on 22/02/2023.
+//
+
+import UIKit
+import CoreData
+
+final class CoreDataManager {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let persistentContainer = NSPersistentContainer(name: "EventsApp")
+        persistentContainer.loadPersistentStores { _, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        return persistentContainer
+    }()
+    
+    var managedObjectContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    func saveEvent(name: String, date: Date, image: UIImage) {
+        let event = Event(context: managedObjectContext)
+        event.setValue(name, forKey: "name")
+        event.setValue(date, forKey: "date")
+        let imageData = image.jpegData(compressionQuality: 1)
+        event.setValue(imageData, forKey: "image")
+        
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func fetchEvents() -> [Event] {
+        do {
+            let fetchRequest = NSFetchRequest<Event>(entityName: "Event")
+            let events = try managedObjectContext.fetch(fetchRequest)
+            return events
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+}
